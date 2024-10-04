bf15fb1cc191a12d1fd960832280685157ccb605
Spectrumzero <lmhspecter@gmail.com>
2024-10-04 13:30:52 +0800

    initial commit: test

--- ch3/UVa272.cpp
+++ ch3/UVa272.cpp
@@ -1,11 +1,16 @@
 // UVa272 Tex Quotes
 // Rujia Liu
-#include<stdio.h>
-int main() { 
+#include <stdio.h>
+int main() {
+  freopen("input/272r.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
   int c, q = 1;
-  while((c = getchar()) != EOF) {
-    if(c == '"') { printf("%s", q ? "``" : "''"); q = !q; }
-    else printf("%c", c);
+  while ((c = getchar()) != EOF) {
+    if (c == '"') {
+      printf("%s", q ? "``" : "''");
+      q = !q;
+    } else
+      printf("%c", c);
   }
   return 0;
 }
